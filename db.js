37376c1d759f29ee2b946382ba4a104fb9df1f6a
Prashant Singh <59484201+prashanthakur@users.noreply.github.com>
2023-04-20 20:31:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('')
+mongoose.connect('mongodb+srv://thakurgmail_com:1239910@cluster0.z32tm.mongodb.net/Edu_August?retryWrites=true&w=majority')
